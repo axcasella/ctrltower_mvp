6b459bf727d7d73e28b82413f125eb046508b955
axcasella <axc@bu.edu>
2023-08-16 14:15:29 -0400

    set up routes and controllers

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import vendorRoutes from './routes/vendors.js';
+import generalRoutes from './routes/general.js';
+import contractManagementRoutes from './routes/contract_management.js';
+import dealsRoutes from './routes/deals.js';
+import settingsRoutes from './routes/settings.js';
+
 // Configuration
 dotenv.config();
 const app = express();
@@ -17,7 +23,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
-app.use("/client", clientRoutes);
-app.use("/general", generalRoutes);
-app.use("/management", managementRoutes);
+// for vendors
+app.use("/vendors", vendorRoutes);
+// for dashboard
+app.use("/general", generalRoutes); 
+// for management of contracts
+app.use("/contract_management", contractManagementRoutes);
+// for deal status
 app.use("/deals", dealsRoutes);
+// for settings
+app.use("/settings", settingsRoutes);
